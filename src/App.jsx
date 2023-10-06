c6675d0ff39467b8f9f497057c87248b8cd55bda
Dennis <emanuele.glorioso@gmail.com>
2023-10-06 16:26:44 +0200

    jsx-01

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const hello = "Hello World!"
 
   return (
     <>
-      
+      <h1>{hello}</h1>
     </>
   )
 }
