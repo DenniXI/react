8d0e276cf356ad8f26cebaa242b48155119aebf1
Dennis <emanuele.glorioso@gmail.com>
2023-10-06 18:25:59 +0200

    jsx-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 function App() {
-  
+  function helloName(name){
+    return <h1>Hello, {name}!</h1>
+  }
 
   return (
     <>
-      
+      {helloName(<h1>GinoPaoli</h1>)}
     </>
   )
 }
