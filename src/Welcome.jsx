1ab0c248fa1717295eb4f1bf408e2613a498db86
Dennis <emanuele.glorioso@gmail.com>
2023-10-07 12:46:59 +0200

    props-19

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,7 +1,7 @@
 export function Welcome({ name, age }) {
     return (
         <>
-            {name === "" && <p>Welcome, Ospite</p>}
+            {name === "" && <p>Welcome,Ospite </p>}
             {name && <p>Welcome, {name}!</p>}
             <p>Your age is {age}</p>
         </>
