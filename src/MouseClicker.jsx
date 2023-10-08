62279ff3bad7beb6b12ad0166d05340d821f247a
Dennis <emanuele.glorioso@gmail.com>
2023-10-09 01:29:57 +0200

    h-e-37

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,15 +1,18 @@
-// Create a MouseClicker component that contains a button with a name prop set to the "one" string. 
-// Attach an event handler to the button that prints the name prop to the console when clicked by reading the event.target.name property.
+//Add an image inside the button and, without creating a new event handler or attaching an event handler to the image, print 
+//the name attribute of the button regardless of whether the button or the image is clicked
 
 export function MouseClicker() {
-    function handler(event){
+    function handler(event) {
         console.log(event.target.name)
     }
 
     return (
         <div>
-            <button name="one" onClick={handler}>Click me!</button>
+            <button name="one" onClick={handler}>
+                <img width={24} heigth={24} />
+                Click me!
+            </button>
         </div>
-        )
+    )
 
 }
