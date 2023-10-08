b6d26ab0d7db8cf5c9518fb4ac41f945f15d7f09
Dennis <emanuele.glorioso@gmail.com>
2023-10-08 22:09:11 +0200

    state-33

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,15 +5,21 @@ export function Counter({initialValue = 0}){
     const [counter, setCounter] = useState(initialValue)
 
     function handleIncrementCounter(){
-        
         setCounter((c) => c + 1)
-        
+    }
+    function handleDecrementCounter(){
+        setCounter((c) => c - 1)
+    }
+    function handleResetCounter(){
+        setCounter((c) => initialValue)
     }
 
     return (
         <div>
             <CounterDisplay counter={counter}/>
             <button onClick={handleIncrementCounter}>Increment</button>
+            <button onClick={handleDecrementCounter}>Decrement</button>
+            <button onClick={handleResetCounter}>Reset</button>
         </div>
     )
 }
