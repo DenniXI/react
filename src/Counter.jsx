744f6956d5ad7dee35f732996a464c9afa794a2a
Dennis <emanuele.glorioso@gmail.com>
2023-10-08 21:49:40 +0200

    state-30

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,7 +4,12 @@ export function Counter(){
     const [counter, setCounter] = useState(0)
 
     function handleIncrementCounter(){
-        setCounter ((c) => c + 1)
+        //è meglio che sia scritta in questo modo per andare "sul sicuro" e perchè permette di inserire piu "setter".
+        
+        // setCounter ((c) => c + 1)
+        
+        //Altrimenti si puo scrivere cosi:
+        setCounter (counter + 1)
     }
 
     return (
