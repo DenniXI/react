95df3fcb0f45fd5688283754a003dbdf0d8d0b43
Dennis <emanuele.glorioso@gmail.com>
2023-10-09 01:38:32 +0200

    h-e-38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,15 +1,15 @@
-//Add an image inside the button and, without creating a new event handler or attaching an event handler to the image, print 
-//the name attribute of the button regardless of whether the button or the image is clicked
+//Add an onClick event handler to the image that prints the src of the image to the console. 
+//Does the name attribute of the button still get printed to the console when the image is clicked? Why?
 
 export function MouseClicker() {
     function handler(event) {
-        console.log(event.target.name)
+        console.log(event.target.src)
     }
 
     return (
         <div>
             <button name="one" onClick={handler}>
-                <img width={24} heigth={24} />
+                <img src="https://th.bing.com/th/id/OIP.NTEFb1S3tNdPGq09gaXwIQHaEo?pid=ImgDet&rs=1"  width={40} heigth={40} />
                 Click me!
             </button>
         </div>
