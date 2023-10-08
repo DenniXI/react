a5e4504c2bd65e943af0cc75742964917d5160c3
Dennis <emanuele.glorioso@gmail.com>
2023-10-09 00:25:54 +0200

    effect-35

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import { Clock } from './Clock'
 import { Counter } from './Counter'
 
 function App() {
 
   return (
     <>
-      <Counter initialValue={10}/>
+      {/* <Counter initialValue={10}/> */}
+      <Clock/>
     </>
   )
 }
