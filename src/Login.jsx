3b5202cb8c1f6303384b904a2d90cbee64dae8a5
Dennis <emanuele.glorioso@gmail.com>
2023-10-11 16:28:37 +0200

    forms-42

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,15 +1,37 @@
-import { Checkbox } from "./Checkbox";
-import { Password } from "./Password";
-import {Username} from "./Username";
-
-export function Login(){
 
 
-    return(
+function createData() {
+    return {
+        username: '',
+        password: '',
+        session: false,
+    }
+}
+
+export function Login() {
+    const [data, setData] = useState(createData())
+
+    function handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        const checked = event.target.checked;
+        const type = event.target.type;
+
+
+        setData((data) => {
+            return {
+                ...data,
+                [name]: type === 'checkbox' ? checked : value,
+            }
+        })
+    }
+
+
+    return (
         <div>
-            <Username/>
-            <Password/>
-            <Checkbox/>
+            <input name="username" value={data.username} onChange={handleChange} />
+            <input name="password" value={data.value} onChange={handleChange} />
+            <input name="checkbox" type="checkbox" value={data.checked} onChange={handleChange} />
         </div>
     )
 }
