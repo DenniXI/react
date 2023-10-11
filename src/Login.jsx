da7af512c9c76c7c254627a5862708dc1a9a91bf
Dennis <emanuele.glorioso@gmail.com>
2023-10-11 18:01:36 +0200

    forms-44

--- src/Login.jsx
+++ src/Login.jsx
@@ -18,13 +18,15 @@ export function Login() {
         const checked = event.target.checked;
         const type = event.target.type;
 
-
         setData((data) => {
             return {
                 ...data,
                 [name]: type === 'checkbox' ? checked : value,
             }
         })
+    }
+    function handleReset() {
+        setData(createData())
     }
 
 
@@ -33,10 +35,11 @@ export function Login() {
             <h2>Username</h2>
             <input name="username" value={data.username} onChange={handleChange} />
             <h2>Password</h2>
-            <input name="password" value={data.value} onChange={handleChange} />
+            <input name="password" value={data.password} onChange={handleChange} />
             <h2>Remember</h2>
-            <input name="checkbox" type="checkbox" value={data.checked} onChange={handleChange} />
+            <input name="session" type="checkbox" checked={data.session} onChange={handleChange} />
             <button disabled={!data.username || !data.password}>Login</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
