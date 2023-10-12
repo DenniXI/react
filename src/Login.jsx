f5178320d7b4bc6e41567e69ceab2280e45cf5ac
Dennis <emanuele.glorioso@gmail.com>
2023-10-12 21:08:16 +0200

    uncontrolled-forms-46

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,53 +1,36 @@
-import { useState } from "react";
-
-
-function createData() {
-    return {
-        username: '',
-        password: '',
-        session: false,
-    }
-}
-
 export function Login() {
-    const [data, setData] = useState(createData())
+    function handleFormSubmit(event) {
+        event.preventDefault()
+        const username = event.target.elements.namedItem('username').value;
+        const password = event.target.elements.namedItem('password').value;
+        const session = event.target.elements.namedItem('session').checked;
 
-    function handleChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-        const checked = event.target.checked;
-        const type = event.target.type;
 
-        setData((data) => {
-            return {
-                ...data,
-                [name]: type === 'checkbox' ? checked : value,
-            }
-        })
+        const data = {
+            username,
+            password,
+            session,
+        }
+        console.log(data)
     }
-
-    function handleReset() {
-        setData(createData())
-    }
-
-    function handleLogin(event) {
-        event.preventDefault();
-        console.log(data.username, data.password, data.session)
-    }
-
     return (
         <div>
-            <form action="#" onSubmit={handleLogin}>
+            <form action="#" onSubmit={handleFormSubmit}>
+
                 <h2>Username</h2>
-                <input name="username" value={data.username} onChange={handleChange} />
+                <input name="username" />
+
                 <h2>Password</h2>
-                <input name="password" value={data.password} onChange={handleChange} />
+                <input name="password" type="password" />
+
                 <h2>Remember</h2>
-                <input name="session" type="checkbox" checked={data.session} onChange={handleChange} />
-                <button disabled={!data.username || !data.password}>Login</button>
-                <button onClick={handleReset}>Reset</button>
+                <input name="session" type="checkbox" />
+
+                <button>Login</button>
+                <button type="reset">Reset</button>
             </form>
 
         </div>
     )
 }
+
