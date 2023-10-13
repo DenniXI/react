2402fbc185fe2bcf2f18756122608564ce4c279d
Dennis <emanuele.glorioso@gmail.com>
2023-10-13 20:57:37 +0200

    ref-51

--- src/FocusableInput.jsx
+++ src/FocusableInput.jsx
@@ -3,9 +3,16 @@ import { useEffect, useRef, useState } from "react"
 export function FocusableInput() {
 
     const [username, setUsername] = useState('')
+    const mountedRef = useRef(false)
     const inputRef = useRef(null)
 
     useEffect(() => {
+        if(!mountedRef.current){
+            mountedRef.current = true
+            console.log('mounted for the first time')
+        }else{
+            console.log('mounted again')
+        }
         inputRef.current?.focus()
     }, [])
 
