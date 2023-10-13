de32fb3e07b36976cd6154c6df83cc9995667b73
Dennis <emanuele.glorioso@gmail.com>
2023-10-13 21:50:36 +0200

    uncontrolled-forms-49

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,25 +2,19 @@ export function Login() {
     function handleFormSubmit(event) {
         event.preventDefault()
 
-        //Questo è il metodo standard per accedere ai valori del form:
-        // const formData = {
-        //     username: event.target.username.value,
-        //     password: event.target.password.value,
-        //     session: event.target.session.checked
-        // }
+        // Vantaggi: semplifica l'accesso ai dati del form.
+        // Svantaggi: ma non offre un modo diretto per manipolare i dati 
+        //in modo avanzato prima dell'invio.
 
-        //Questo è un metodo non standard
-        // const formData = new FormData(event.target)
-        // const username =  formData.get('username');
-        // const password = formData.get('password');
-        // const session = formData.get('session');
-        
+        const formData = new FormData(event.target)
+        const data = {
+            username: formData.get('username'),
+            password: formData.get('password'),
+            session: formData.get('session'),
+        }
 
-        const formData = {
-            username: document.forms[0].elements.username.value,
-            password: document.forms[0].elements.password.value,
-        };
-        console.log(formData)
+
+        console.log(data)
     }
     return (
         <div>
