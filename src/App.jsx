171829fc7ddf91660a0de6452046bb63ec0c9f75
Dennis <emanuele.glorioso@gmail.com>
2023-10-22 22:54:20 +0200

    useCallback-80

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import {useCurrentLocation} from './UseCurrentLocation';
+import { Counter } from './Counter';
 
 function App() {
-  const { location, error, loading, getCurrentLocation } = useCurrentLocation();
 
   return (
     <div>
-      <h1>Current Location</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {location && (
-        <div>
-          <p>Latitude: {location.latitude}</p>
-          <p>Longitude: {location.longitude}</p>
-        </div>
-      )}
-      <button onClick={getCurrentLocation}>Get Current Location</button>
+      <Counter/>
     </div>
   );
 }
--- src/UseCounter.jsx
+++ src/UseCounter.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export function useCounter(initialValue = 0){
 
     const [counter, setCounter] = useState(initialValue)
 
-    function handleIncrementCounter() {
+    const handleIncrementCounter = useCallback(()=> {
         setCounter((c) => c + 1)
-    }
+    },[counter])
 
-    function handleDecrementCounter() {
+    const handleDecrementCounter = useCallback(()=> {
         setCounter((c) => c - 1)
-    }
+    }, [counter])
 
-    function handleResetCounter() {
+    const handleResetCounter = useCallback(() => {
         setCounter(initialValue)
-    }
+    }, [])
 
     return {
         counter: counter,
