eebb37c48dd71afa4d584eb290016bc5272009d3
Dennis <emanuele.glorioso@gmail.com>
2023-10-22 22:46:46 +0200

    some try

--- src/UseGithubUser.jsx
+++ src/UseGithubUser.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export function useGithubUser(username){
+export function useGithubUser(username) {
     const [user, setUser] = useState(null)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
@@ -10,19 +10,24 @@ export function useGithubUser(username){
             try {
                 const response = await fetch(`https://api.github.com/users/${username}`)
                 if (!response.ok) {
-                    console.log('error into fetch response')
+                    setError(new Error('Error into fetch response'))
+                    setLoading(false)
                 } else {
                     const data = await response.json()
                     setUser(data)
+                    setError(error)
+                    setLoading(false)
                     console.log(user)
                 }
             }
             catch (error) {
-            console.log(error)}
+                setError(new Error("Error into try-catch"))
+                setLoading(false);
+            }
         }
-         
+
         fetchGit()
     }, [username])
 
-    return {user}
+    return { user, error, loading }
 }
