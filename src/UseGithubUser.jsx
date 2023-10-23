ee05e32458abd0e38877c81f8fc484cb7187a110
Dennis <emanuele.glorioso@gmail.com>
2023-10-23 11:39:34 +0200

    change usegithub

--- src/UseGithubUser.jsx
+++ src/UseGithubUser.jsx
@@ -1,27 +1,31 @@
 import { useEffect, useState } from "react"
 
-export function useGithubUser(username){
+export function useGithubUser(username) {
     const [user, setUser] = useState(null)
-    
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
+
         async function fetchGit() {
             try {
                 const response = await fetch(`https://api.github.com/users/${username}`)
                 if (!response.ok) {
-                    console.log('error into fetch response')
+                    setError(new Error('Error into fetch response'))
                 } else {
                     const data = await response.json()
                     setUser(data)
+                    setError(error)
                     console.log(user)
                 }
             }
             catch (error) {
-            console.log(error)}
+                setError(new Error("Error into try-catch"))
+            }
+            finally {
+                setLoading(false)
+            }
         }
-         
-        fetchGit()
-    }, [username])
 
-    return {user}
+    
+    return { user, error, loading, fetchGit }
 }
