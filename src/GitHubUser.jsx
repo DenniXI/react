ae62b7b32d1066c386d08830096a23c94f690fb9
Dennis <emanuele.glorioso@gmail.com>
2023-10-23 11:26:21 +0200

    hooks-78

--- src/GitHubUser.jsx
+++ src/GitHubUser.jsx
@@ -2,18 +2,22 @@ import { useEffect, useState } from "react";
 import { useGithubUser } from "./UseGithubUser";
 
 export function GitHubUser({ username }) {
-    const { user } = useGithubUser(username)
-    if (!user) {
-        return <h1>Loading...</h1>;
-    } else {
-        return (
-            <div>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error: {error.message}</p>}
-                <h1>{user.name}</h1>
-                <h2>{user.login}</h2>
-                <img src={user.avatar_url} />
-            </div>
-        )
-    }
+    const { user, loading, error, fetchGit } = useGithubUser(username)
+
+    return (
+        <div>
+            <button onClick={fetchGit}>Fetch dei dati</button>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+
+            {user && (
+                <div>
+                    <h1>{user.name}</h1>
+                    <h2>{user.login}</h2>
+                    <img src={user.avatar_url} />
+                </div>
+            )}
+
+        </div>
+    )
 }
--- src/UseGithubUser.jsx
+++ src/UseGithubUser.jsx
@@ -5,29 +5,27 @@ export function useGithubUser(username) {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
+
         async function fetchGit() {
             try {
                 const response = await fetch(`https://api.github.com/users/${username}`)
                 if (!response.ok) {
                     setError(new Error('Error into fetch response'))
-                    setLoading(false)
                 } else {
                     const data = await response.json()
                     setUser(data)
                     setError(error)
-                    setLoading(false)
                     console.log(user)
                 }
             }
             catch (error) {
                 setError(new Error("Error into try-catch"))
-                setLoading(false);
+            }
+            finally {
+                setLoading(false)
             }
         }
 
-        fetchGit()
-    }, [username])
-
-    return { user, error, loading }
+    
+    return { user, error, loading, fetchGit }
 }
