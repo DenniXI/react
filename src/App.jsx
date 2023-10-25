22e8ca3cd414ab19e3df8c682a595101fef46bbb
Dennis <emanuele.glorioso@gmail.com>
2023-10-25 15:49:43 +0200

    Path not found

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,16 @@ function App() {
   return (
     <div>
       <Routes>
-      <Route path='/' element={<Welcome/>}/>
-      <Route path='/Counter' element={<Counter/>}/>
-      <Route path='users/:username' element={<ShowGithubUser/>}/>
-    </Routes>
-    <div>
-      <Link to='/'>Welcome</Link>
-      <hr />
-      <Link to='/Counter'>Counter</Link>
-      <hr />
-      <Link to='users/:username'>User</Link>
-    </div>
+        <Route path='*' element={<h1>Path Not Found</h1>} />
+        <Route path='/' element={<Welcome />} />
+        <Route path='/Counter' element={<Counter />} />
+        <Route path='users/:username' element={<ShowGithubUser />} />
+      </Routes>
+      <div>
+        <Link to='/'>Welcome</Link> |
+        <Link to='/Counter'>Counter</Link> |
+        <Link to='users/:username'>User</Link>
+      </div>
 
     </div>
   );
