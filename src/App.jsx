7620e81f5ea77d1b276248400589ec71eb5a7129
Dennis <emanuele.glorioso@gmail.com>
2023-10-25 09:48:26 +0200

    ReactRouter-83

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom"
 import {useCurrentLocation} from './UseCurrentLocation';
 import { Welcome } from './Welcome';
+import {Counter} from './Counter'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Welcome/>}/>
+      <Route path='/Counter' element={<Counter/>}/>
     </Routes>
   );
 }
