3ab74da85f5feb1d900157efaaf1be930d049099
Dennis <emanuele.glorioso@gmail.com>
2023-10-29 11:39:52 +0100

    swr-90

--- src/UseGithubUser.jsx
+++ src/UseGithubUser.jsx
@@ -4,11 +4,15 @@ import useSWR from "swr"
 const fetcher = (url)=> fetch(url).then((response) => response.json())
 
 export function useGithubUser(username) {
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const shouldFetch = username !== null && username !== undefined
+
+    const { data, error } = useSWR(
+        shouldFetch ? `https://api.github.com/users/${username}` : null, fetcher
+        );
     
     return { 
         user: data, 
         error, 
-        isLoading: !data && !error,
+        isLoading: shouldFetch && !data && !error,
     }
 }
